b54c37aa304612768f3be5e7aa189f87e8abaee0
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-08 23:28:17 +0300

    Loading screen edited.

--- [qb]/qb-loading/assets/scripts/config.js
+++ [qb]/qb-loading/assets/scripts/config.js
@@ -1,12 +1,12 @@
 Config = {}; // Don't touch
 
-Config.ServerIP = "YOUR IP SERVER";
+Config.ServerIP = "185.250.210.76";
 
 // Social media buttons on the left side
 Config.Socials = [
-    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/cKt4Mpd2PQ"},
-    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "#"},
-    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "#"},
+    {name: "discord", label: "Discord", description: "Buraya tıklayarak discord adresini kopyalayabilirsin!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/cKt4Mpd2PQ"},
+    {name: "instagram", label: "Instagram", description: "Buraya tıklayarak instagram adresini kopyalayabilirsin!!", icon: "assets/media/icons/tiktok.png", link: "#"},
+    {name: "tebex", label: "Donations", description: "Bağış", icon: "assets/media/icons/tebex.png", link: "#"},
 ];
 
 Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
@@ -14,10 +14,7 @@ Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in 
 
 // Staff list
 Config.Staff = [
-    {name: "NaorNC #1", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
-    {name: "Owner #2", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
-
-
+    {name: "Vidarr#6199", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
 ];
 
 // Categories
