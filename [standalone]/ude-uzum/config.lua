a8263f3857e3318e9ef8f96f2ceeb42ffeb0f4eb
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-08 23:43:02 +0300

    Üzüm scripti v.0

---
+++ [standalone]/ude-uzum/config.lua
@@ -0,0 +1,23 @@
+----- DISCORD: Jew#1881 -----
+
+Config = {}
+
+Config.WeedLocation = { -- KENEVIRLERIN CIKACAGI YERLER
+    vector3(-1823.05, 2157.0, 115.2),
+    vector3(-1820.21, 2147.77, 117.73),
+    vector3(-1809.71, 2147.57, 118.92),
+    vector3(-1802.54, 2157.65, 116.76),
+    vector3(-1792.11, 2145.98, 121.41),
+}
+
+Config.drugBaseName = "grape"
+Config.drugProcessedName = "wine"
+Config.drugPerPrice = 500
+Config.process = vector3(-1517.54, 1502.54, 111.62) -- KENEVIRIN ISLENECEGI YER
+
+Config.Blips = { -- BLIPLER
+    { title = "Şarap Taciri", colour = 27, id = 1, x = -2186.61, y = -400.44, z = 13.27 },
+    { title = "Üzüm Bağı", colour = 27, id = 1, x = -1825.52, y = 2152.84, z = 116.3 },
+    { title = "Üzüm İşleme", colour = 27, id = 1, x = -1518.69, y = 1503.06, z = 111.62 },
+}
+
