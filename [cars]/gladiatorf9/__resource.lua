24c96eaf4adcfe388e19784250b84483e4ec4618
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-13 07:28:32 +0300

    fladiatorf9 eklendi

---
+++ [cars]/gladiatorf9/__resource.lua
@@ -0,0 +1,27 @@
+resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+-- MADE BY PROJECT ROGUE ANDREW | ABOWPLAYS
+description "Car Add-on"
+
+files {
+  "data/**/handling.meta",
+  "data/**/vehiclelayouts.meta",
+  "data/**/vehicles.meta",
+  --"data/**/carcols.meta",
+  "data/**/carvariations.meta",
+  -- SES
+
+--  "audio/**/s55b30.dat151.rel",
+--  "audio/**/s55b30_sounds.dat54.rel",
+--  "audio/**/dlc_s55b30/s55b30.awc",
+--  "audio/**/dlc_s55b30/s55b30_npc.awc"
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+--data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+-- SES
+--data_file "AUDIO_GAMEDATA" "audio/**/s55b30_game.dat"
+--data_file "AUDIO_SOUNDDATA" "audio/**/s55b30_sounds.dat"
+--data_file "AUDIO_WAVEPACK" "audio/**/s55b30"
