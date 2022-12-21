224d48e6b05fb41f86b24ccb0ae5c3d46e99238e
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-21 19:38:39 +0300

    başlangıçta bisiklet verme

---
+++ [qb]/qb-scooterrental/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.PedModel = 'a_m_m_prolhost_01'
+Config.PedLocation = vector4(-1026.5, -2730.08, 12.76, 156.28)
+Config.SpawnLocation = vector4(-1024.74, -2728.52, 13.66, 236.26)
+
+Config.ScooterModel = "Cruiser"
+Config.ScooterPrice = 150
+
+
+Config.FuelSystem = "legacyfuel"
