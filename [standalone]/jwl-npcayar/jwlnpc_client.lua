0b68340418bf399b325d38c6fea86ee360efdac4
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-24 00:32:23 +0300

    Update jwlnpc_client.lua

--- [standalone]/jwl-npcayar/jwlnpc_client.lua
+++ [standalone]/jwl-npcayar/jwlnpc_client.lua
@@ -6,7 +6,7 @@
    /      |______________________________________________|     \
   /__________)                                        (_________\
 
-]]--
+]] --
 
 local relationshipTypes = {
 	'GANG_1',
@@ -53,12 +53,12 @@ Citizen.CreateThread(function()
 end)
 
 Citizen.CreateThread(function()
-   while true do
-     Citizen.Wait(1)
-     RemoveAllPickupsOfType(0xDF711959)
-     RemoveAllPickupsOfType(0xF9AFB48F)
-     RemoveAllPickupsOfType(0xA9355DCD)
-   end
+	while true do
+		Citizen.Wait(1)
+		RemoveAllPickupsOfType(0xDF711959)
+		RemoveAllPickupsOfType(0xF9AFB48F)
+		RemoveAllPickupsOfType(0xA9355DCD)
+	end
 end)
 
 -- AYAR KISMI
@@ -67,9 +67,9 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0) -- crash engelleme
 
-		SetVehicleDensityMultiplierThisFrame(0.0) -- trafik yoğunluğunu ayarla (max 1)
-		SetPedDensityMultiplierThisFrame(0.0) -- npc yoğunluğunu ayarla (max 1)
-		SetRandomVehicleDensityMultiplierThisFrame(0.0) -- rastgele araçları (araba senaryoları / park yerinden çıkan arabalar vb.) ayarla (max 1)
+		SetVehicleDensityMultiplierThisFrame(0.1) -- trafik yoğunluğunu ayarla (max 1)
+		SetPedDensityMultiplierThisFrame(0.5) -- npc yoğunluğunu ayarla (max 1)
+		SetRandomVehicleDensityMultiplierThisFrame(0.05) -- rastgele araçları (araba senaryoları / park yerinden çıkan arabalar vb.) ayarla (max 1)
 		SetParkedVehicleDensityMultiplierThisFrame(0.0) -- rastgele park etmiş araçları (park etmiş araba senaryoları) ayarla (max 1)
 		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0) -- rastgele npc pedlerini veya senaryo pedlerini ayarla (max 1)
 		SetGarbageTrucks(false) -- çöp kamyonlarının rastgele spawnlanmasını durdurun (true/false)
@@ -77,9 +77,9 @@ Citizen.CreateThread(function()
 		SetCreateRandomCops(false) -- etrafta dolaşan / araba kullanan rastgele polisleri devre dışı bırakın (true/false)
 		SetCreateRandomCopsNotOnScenarios(false) -- rastgele polislerin (bir senaryoda değil) ortaya çıkmasını durdurun (true/false)
 		SetCreateRandomCopsOnScenarios(false) -- rastgele polislerin (bir senaryoda) spawnlanmasını durdurun (true/false)
-		
-		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
-		ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
-		RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
+
+		local x, y, z = table.unpack(GetEntityCoords(PlayerPedId()))
+		-- ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+		-- RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
 	end
-end) 
+end)
