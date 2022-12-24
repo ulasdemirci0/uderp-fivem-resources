731de698e6136faccf5e5215537453a7ffc3005f
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2022-12-24 21:42:22 +0300

    qb bank eksiye düşme kapatıldı

--- [qb]/qb-core/config.lua
+++ [qb]/qb-core/config.lua
@@ -7,7 +7,7 @@ QBConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in mil
 
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = { cash = 250, bank = 650, crypto = 0 } -- type = startamount - Add or remove money types for your server (for ex. blackmoney = 0), remember once added it will not be removed from the database!
-QBConfig.Money.DontAllowMinus = { 'cash', 'crypto' } -- Money that is not allowed going in minus
+QBConfig.Money.DontAllowMinus = { 'cash', 'crypto', 'bank' } -- Money that is not allowed going in minus
 QBConfig.Money.PayCheckTimeOut = 10 -- The time in minutes that it will give the paycheck
 QBConfig.Money.PayCheckSociety = true -- If true paycheck will come from the society account that the player is employed at, requires qb-management
 
